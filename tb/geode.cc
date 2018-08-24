3e5752bc663067b41a811bc970984e98583f0375
Michael Davies <michaelstoby@gmail.com>
2018-08-23 23:29:46 -0500

    Cleanup testbench a bit

--- tb/geode.cc
+++ tb/geode.cc
@@ -14,64 +14,56 @@ double sc_time_stamp() {
     return simtime;
 }
 
-void HandleIMem(VGeodeCore * top, uint8_t * mem, uint64_t raddr, uint8_t ren) {
-    uint64_t addr;
+typedef struct _CaptureSignals {
+    uint64_t i_raddr;
+    uint8_t i_ren;
+    uint64_t d_raddr;
+    uint8_t d_ren;
+    uint64_t d_waddr;
+    uint64_t d_wdata;
+    uint8_t d_wen;
+} CaptureSignals;
 
-    if (ren) {
-        addr = raddr;
+void HandleCapture(VGeodeCore * top, CaptureSignals& signals) {
+    signals.i_raddr = top->io_imem_r_addr;
+    signals.i_ren = top->io_imem_r_en;
+    signals.d_raddr = top->io_dmem_r_addr;
+    signals.d_ren = top->io_dmem_r_en;
+    signals.d_waddr = top->io_dmem_w_addr;
+    signals.d_wdata = top->io_dmem_w_data;
+    signals.d_wen = top->io_dmem_w_en;
+}
 
-        if (addr < MEMSIZE - 4) {
-            top->io_imem_r_data =
-                ((uint32_t) mem[addr + 3]) << 24 |
-                ((uint32_t) mem[addr + 2]) << 16 |
-                ((uint32_t) mem[addr + 1]) << 8 |
-                ((uint32_t) mem[addr + 0]);
+void HandleIMem(VGeodeCore * top, uint8_t * mem, CaptureSignals& signals) {
+    if (signals.i_ren) {
+        if (signals.i_raddr < MEMSIZE - 4) {
+            top->io_imem_r_data = *((uint32_t *)(mem + signals.i_raddr));
         }
         else {
-            printf("Invalid IMem Read: Addr = %lx\n", addr);
+            printf("Invalid IMem Read: Addr = %lx\n", signals.i_raddr);
             top->io_imem_r_data = 0;
         }
     }
 }
 
-void HandleDMem(VGeodeCore * top, uint8_t * mem, uint64_t raddr, uint8_t ren, uint64_t waddr, uint64_t wdata, uint8_t wen) {
-    uint64_t addr;
+void HandleDMem(VGeodeCore * top, uint8_t * mem, CaptureSignals& signals) {
 
-    if (ren) {
-        addr = raddr;
-
-        if (addr < MEMSIZE - 8) {
-            top->io_dmem_r_data =
-                ((uint64_t) mem[addr + 7]) << 56 |
-                ((uint64_t) mem[addr + 6]) << 48 |
-                ((uint64_t) mem[addr + 5]) << 40 |
-                ((uint64_t) mem[addr + 4]) << 32 |
-                ((uint64_t) mem[addr + 3]) << 24 |
-                ((uint64_t) mem[addr + 2]) << 16 |
-                ((uint64_t) mem[addr + 1]) << 8 |
-                ((uint64_t) mem[addr + 0]);
+    if (signals.d_ren) {
+        if (signals.d_raddr < MEMSIZE - 8) {
+            top->io_dmem_r_data = *((uint64_t *)(mem + signals.d_raddr));
         }
         else {
-            printf("Invalid DMem Read: Addr = %lx\n", addr);
+            printf("Invalid DMem Read: Addr = %lx\n", signals.d_raddr);
             top->io_imem_r_data = 0;
         }
     }
 
-    if (wen) {
-        addr = waddr;
-
-        if (addr < MEMSIZE - 8) {
-            mem[addr + 7] = (wdata >> 56) & 0xFF;
-            mem[addr + 6] = (wdata >> 48) & 0xFF;
-            mem[addr + 5] = (wdata >> 40) & 0xFF;
-            mem[addr + 4] = (wdata >> 32) & 0xFF;
-            mem[addr + 3] = (wdata >> 24) & 0xFF;
-            mem[addr + 2] = (wdata >> 16) & 0xFF;
-            mem[addr + 1] = (wdata >> 8) & 0xFF;
-            mem[addr + 0] = (wdata) & 0xFF;
+    if (signals.d_wen) {
+        if (signals.d_waddr < MEMSIZE - 8) {
+            *((uint64_t *)(mem + signals.d_waddr)) = signals.d_wdata;
         }
         else {
-            printf("Invalid DMem Write: Addr = %lx\n", addr);
+            printf("Invalid DMem Write: Addr = %lx\n", signals.d_waddr);
             top->io_imem_r_data = 0;
         }
     }
@@ -95,13 +87,8 @@ int main(int argc, char **argv) {
     //
     // Output signals sampled before clock edge
     //
-    uint64_t i_raddr;
-    uint8_t i_ren;
-    uint64_t d_raddr;
-    uint8_t d_ren;
-    uint64_t d_waddr;
-    uint64_t d_wdata;
-    uint8_t d_wen;
+
+    CaptureSignals signals;
 
     top->io_reset = 1;
     for (int i = 0; i < 10; i++) {
@@ -120,27 +107,24 @@ int main(int argc, char **argv) {
 
     while (simtime < 1000) {
         top->io_clock = 0;
-        top->eval();
-        vcd->dump(simtime++);
+
         top->eval();
         vcd->dump(simtime++);
 
-        i_raddr = top->io_imem_r_addr;
-        i_ren = top->io_imem_r_en;
-        d_raddr = top->io_dmem_r_addr;
-        d_ren = top->io_dmem_r_en;
-        d_waddr = top->io_dmem_w_addr;
-        d_wdata = top->io_dmem_w_data;
-        d_wen = top->io_dmem_w_en;
+        top->eval();
+        vcd->dump(simtime++);
+
+        HandleCapture(top, signals);
 
         top->io_clock = 1;
+
         top->eval();
-        HandleIMem(top, mem, i_raddr, i_ren);
-        HandleDMem(top, mem, d_raddr, d_ren, d_waddr, d_wdata, d_wen);
-        vcd->dump(simtime++);
-        top->eval();
+        HandleIMem(top, mem, signals);
+        HandleDMem(top, mem, signals);
         vcd->dump(simtime++);
 
+        top->eval();
+        vcd->dump(simtime++);
     }
 
     vcd->close();
